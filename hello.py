0a71fafdc93c9b3d97e081ac2e0b949a1f5bac41
Ajit Singh <ajitsinghoriginal@gmail.com>
2026-01-02 15:16:17 +0530

    learning

--- hello.py
+++ hello.py
@@ -8,9 +8,41 @@ import sys
 # print(3)
 # print(4+5)
 # print(3-1)
-print("I am", 29, "years old.")
-"""
-This is
-a multi-line
-comment
-"""
+# print("I am", 29, "years old.")
+# """
+# This is
+# a multi-line
+# comment
+# """
+
+# x = 5
+# y = "test"
+# print(x)
+# print(y)
+
+# casting
+# x = str(3)
+# # y = int(3)
+# # z = float(3)
+# # m = 3
+# # X = 5
+# print(type(x), x, type(y), y, type(z), z, type(m), m, X)
+# x, y, z = "Ajit", "Bruce", "Super"
+# x, y, z = ["Ajit", "Bruce", "Super"]
+# names = ["Ajit", "Bruce", "Super"]
+# x, y, z = names
+# x = y = z = "Ajit"
+# y = "Singh"
+# print(x, y, z)
+# x = 5
+# y = "Ajit"
+# print(x,  y)
+# print(x)
+
+# x = "Ajit is awesome"
+def myFunc():
+    global x
+    x = "new value"
+    print(x)
+myFunc()
+print(x)
