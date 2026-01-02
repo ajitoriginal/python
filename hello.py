8398d47d2b3d9d8150114257e339f2cb161ced65
Ajit Singh <ajitsinghoriginal@gmail.com>
2026-01-02 14:04:18 +0530

    learning

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ import sys
 
 # print("Hello, Ajit!")
 # print(sys.version)
-print("first"); print("second")
+# print("first"); print("second")
 
 # print("first", end=" "); print("second")
+# print(3)
+# print(4+5)
+# print(3-1)
+print("I am", 29, "years old.")
